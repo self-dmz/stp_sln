1652f4605119a1cd552b6aec9e31f6afb00cd590
this.dmz <this.dmz@mail.ru>
2022-12-21 14:10:02 +0300

    LoginPage is added to test_main_page.py :: test_guest_can_go_to_login_page

--- part_four_page_object/test_main_page.py
+++ part_four_page_object/test_main_page.py
@@ -1,17 +1,19 @@
 import time
 
 import pytest
-
+from pages.login_page import LoginPage
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    wl = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer '
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
+    #wl = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer '
+    page = MainPage(browser, link)
+    page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
     time.sleep(3)
 
 
