4949e0b8fabad499c72b12765ec0def8e0ccc9de
this.dmz <this.dmz@mail.ru>
2022-12-21 12:25:59 +0300

    main_page.py [go_to_login_page] + [test_guest_can_go_to_login_page] in test_main_page.py added.

--- part_four_page_object/test_main_page.py
+++ part_four_page_object/test_main_page.py
@@ -1,10 +1,18 @@
 import time
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()
+    time.sleep(3)
 
 
 def test_add_to_basket_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     btn = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
     time.sleep(3)
